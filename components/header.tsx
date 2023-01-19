14c37ac6e41645ab0ab7a4b77de0741fb237fad0
tzrtg <tzrtg@protonmail.com>
2023-01-19 11:58:16 -0800

    next auth github working

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+// import Link from "next/link";
+import React, { FunctionComponent, ReactNode } from "react";
+
+export default function Header() {
+  return (
+    <>
+
+    </>
+  );
+}
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,18 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 // import DiscordProvider from "next-auth/providers/discord";
-import GithubProvider from "next-auth/providers/github"
-
+import GithubProvider from "next-auth/providers/github";
 
 import { env } from "../../../env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
+    async session({ session, token }) {
+      // if (session.user) {
+      //   console.log("session ", session);
+      //   console.log("token ", token);
+      //   // session.user.id = user.id;
+      // }
       return session;
     },
   },
