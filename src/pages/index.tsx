a20076fa346018479a271d7eb48b4ffdc30a5aac
tzrtg <tzrtg@protonmail.com>
2023-01-19 22:21:24 -0800

    switch theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import Layout from "../../components/layout";
-import Login from "../../components/login";
+import Layout from "@/components/layout";
+import Login from "@/components/login";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Layout>
-        <Login/> 
-      </Layout>
+      <Login />
     </>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    // fontFamily: {
+    //   sans: ['Josefin Sans', 'sans-serif']
+    // },
+    typography: (theme) => ({}),
+    colors: {
+      white: "#FFFFFF"
+    },
+    extend: {
+      colors: {
+        'blackGradient': '#202730',
+        'darkBlue': '#272e38',
+        'mediumGray': '#7c8798',
+        'pureWhite': '#FFF',
+        'orange': '#f77615',
+        'lightGray': '#969fad'
+      }
+    }
+
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
